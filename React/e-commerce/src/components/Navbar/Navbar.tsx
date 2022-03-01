d865cb76dc600b9a656f8de23653f20a74c9e723
shafiq-ahmed <shafiqahmd3@gmail.com>
2022-03-01 15:04:52 +0600

    Search bar added to home navbar

--- React/e-commerce/src/components/Navbar/Navbar.tsx
+++ React/e-commerce/src/components/Navbar/Navbar.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import {Button, Col, Input, Nav, NavbarToggler, Row} from "reactstrap";
 import  "./navbar.css";
+import icon from "./images/search.svg";
+
+
 
 
 
@@ -12,55 +15,22 @@ function Navbar(){
     return(
         <div>
             
-            <div className='container-md'>
-             
-                <nav className='navbar navbar-expand-md navbar-light'>
-
-
-                      
-                   
-                        <a href='#' className='navbar-brand logo'>Dokan</a>
-                       
-                        <form className=' form-line bar'>
-                                <input 
-                                className='form-control ' 
-                                type='search' 
-                                placeholder='Search'
-                                aria-label='Search'
-                                ></input>
-                        </form>
-                         
-                        <button
-                            className='navbar-toggler'
-                            type='button'
-                            data-bs-toggle="collapse"
-                            data-bs-target='#toggleMobileMenu'
-                            aria-controls='toggleMobileMenu'
-                            aria-expanded='false'
-                            aria-label='Toggle Navigation'
-                        >
-                            <span className='navbar-toggler-icon'></span>
-                        </button>
-                    
-                   
-                    <div  className='collapse navbar-collapse' id='toggleMobileMenu'>
-                        <ul className='nav navbar-nav navbar-dark ms-auto'>
-                            <li className='nav-item'>
-                                <a className='nav-link' href=' #'>Home</a>
-                            </li>
-                            <li className='nav-item'>
-                                <a className='nav-link' href='#'>SignUp/Login</a>
-                            </li>
-                            <li className='nav-item'>
-                                <a className='nav-link' href='#'>Contact</a>
-                            </li>
-                        </ul>
-                    </div>
-                       
-                        
+           <div className='nav-body md'>
+                <h5> Dokan</h5>
+                <div className='search-box'>
+                    <input className='search-bar' type='text' placeholder='Type to search'></input>
+                    <a className='search-btn'>
+                    <img src={icon}/>
+                    </a>
+                </div>
+                <nav>
+                    <ul className='nav-links'>
+                        <li><a href='#' >Home</a></li>
+                        <li><a href='#' >Contact</a></li>
+                        <li><a href='#' >About</a></li>
+                    </ul>
                 </nav>
-            </div>
-            
+            </div> 
         </div>
     )
 }
