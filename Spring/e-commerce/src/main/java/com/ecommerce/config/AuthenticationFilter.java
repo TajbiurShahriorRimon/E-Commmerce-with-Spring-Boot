669e4bd3fcb98316879c66e919ed58ab2d3e30c2
shafiq-ahmed <shafiqahmd3@gmail.com>
2022-05-12 15:54:39 +0600

    Authentication Filter added

---
+++ Spring/e-commerce/src/main/java/com/ecommerce/config/AuthenticationFilter.java
@@ -0,0 +1,28 @@
+package com.ecommerce.config;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class AuthenticationFilter extends OncePerRequestFilter{
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		String requestTokenHeader=request.getHeader("Authorization");
+		String username=null;
+		String jwtToken=null;
+		if(requestTokenHeader!=null && requestTokenHeader.startsWith("Bearer ")) {
+			
+			jwtToken=requestTokenHeader.substring(7);
+		}
+	}
+
+}
