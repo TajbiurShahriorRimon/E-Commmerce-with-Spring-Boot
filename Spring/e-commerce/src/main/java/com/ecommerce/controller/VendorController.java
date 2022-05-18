261d64929c8c1435500f4e2f07a9c3b479f890d8
shafiq-ahmed <shafiqahmd3@gmail.com>
2022-05-18 12:11:44 +0600

    Vendor user and shop reg complete

--- Spring/e-commerce/src/main/java/com/ecommerce/controller/VendorController.java
+++ Spring/e-commerce/src/main/java/com/ecommerce/controller/VendorController.java
@@ -1,14 +1,21 @@
 package com.ecommerce.controller;
 
+
+
+import java.net.http.HttpRequest;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
 
+import com.ecommerce.entity.User;
 import com.ecommerce.entity.Vendor;
 import com.ecommerce.service.VendorService;
 
@@ -18,11 +25,26 @@ public class VendorController {
 	@Autowired
 	private VendorService vendorService;
 	
-	@PostMapping(value="/uVendorReg")
-	public ResponseEntity<Vendor> addVendor(@Valid @RequestBody Vendor vendor){
+	
+	
+	@PostMapping(value="/uVendorReg",consumes=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Vendor> addVendor( @RequestBody Vendor vendor) {
 		
+		System.out.println("Vendor");
+		System.out.println(vendor);
 		vendor=this.vendorService.addVendor(vendor);
-		return new ResponseEntity<Vendor>(vendor, HttpStatus.CREATED);
+		
+		return new ResponseEntity<Vendor> (vendor,HttpStatus.CREATED);
 	}
+	
+//	@PostMapping(value="/uVendorReg")
+//	public String addVendor(String id){
+//		
+//		System.out.println("Vendor");
+////		System.out.println(vendor);
+////		vendor=this.vendorService.addVendor(vendor);
+//		
+//		return "id";
+//	}
 
 }
