03c6e2d01843d8c7d9a99319de2291010cf11f8e
shafiq-ahmed <shafiqahmd3@gmail.com>
2022-06-07 16:37:06 +0600

    Image updated

--- React/e-commerce/src/components/Body/SiteHome.tsx
+++ React/e-commerce/src/components/Body/SiteHome.tsx
@@ -77,13 +77,7 @@ class SiteHome extends Component<any, any>{
             <div>
                 <div className="container">
                     <section id="header" className="jumbotron text-left text-white">
-                        <div className="container p-5 ">
-                            <h1 className="display-4 ">GET UPTO 60% OFF!</h1>
-                            <p className="lead">Summer sale is now available with free shipping of all your products.</p>
-                            <hr className="my-4"/>
-                            <p>Patronize us today because limited products available</p>
-                            <a className="btn btn-primary btn-lg " href="#" role="button ">Learn more</a>
-                        </div>
+                        
                     </section> <br/>
                     <div className="row justify-content-lg-start">
                         {resultTable}
